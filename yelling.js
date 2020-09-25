82332ead7022ec720b64cc76dfc588409ecc302d
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-25 19:02:23 -0400

    Complete yelling exercise.

---
+++ yelling.js
@@ -0,0 +1,5 @@
+strings = ['What do you think of this color?', 'Lets play COD.', 'Coding is awesome!', 'Ruby is my fav language.'];
+
+for (string of strings) {
+  console.log(string.toUpperCase());
+}
